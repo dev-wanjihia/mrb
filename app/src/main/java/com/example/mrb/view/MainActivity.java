b5f6e7df944e72845b5e69048a171e24d662ed28
Samson Wanjihia Chege <SCHEGE3@safaricom.co.ke>
2019-09-24 14:52:21 +0300

    Add retrofit, dagger and rx java dependencies
    These dependencies are for:
    - retrofit: making api calls and converting from json to pojo
    - dagger: dependency injection for retrofit and tests to enforce SOLID
    deign pattern
    - rx: reactive programming

--- app/src/main/java/com/example/mrb/view/MainActivity.java
+++ app/src/main/java/com/example/mrb/view/MainActivity.java
@@ -3,14 +3,46 @@ package com.example.mrb.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.mrb.R;
 
-public class MainActivity extends AppCompatActivity {
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity implements Observer<String> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable.just("one", "two", "three")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this);
+    }
+
+    @Override
+    public void onSubscribe(Disposable d) {
+
+    }
+
+    @Override
+    public void onNext(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onError(Throwable e) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
     }
 }
